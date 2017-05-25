2fcccc5af66566bbe471be11c83e9904c99d6f07
oleksandrpetryk <o.petryk@dinamicka.com>
2017-05-25 21:28:48 +0300

    update

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ require('jquery.easing');
     $('.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
